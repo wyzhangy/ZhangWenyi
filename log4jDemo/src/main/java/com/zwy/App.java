ec78581425d0518d56289498b39a9b1a78350944
Your Name <you@example.com>
2018-04-17 10:59:41 +0800

    change

---
+++ log4jDemo/src/main/java/com/zwy/App.java
@@ -0,0 +1,37 @@
+package com.zwy;
+import org.apache.log4j.Logger;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    private static Logger logger = Logger.getLogger(App.class);
+    public static void main(String[] args) {
+        logger.error("error error error error error error error error error error error error ");
+        logger.error("error error error error error error error error error error error error ");
+        logger.error("error error error error error error error error error error error error ");
+        logger.error("error error error error error error error error error error error error ");
+        logger.error("error error error error error error error error error error error error ");
+        logger.error("error error error error error error error error error error error error ");
+        logger.error("error error error error error error error error error error error error ");
+        logger.error("error error error error error error error error error error error error ");
+        logger.error("error error error error error error error error error error error error ");
+        logger.error("error error error error error error error error error error error error ");
+        logger.error("error error error error error error error error error error error error ");
+        logger.error("error error error error error error error error error error error error ");
+        logger.error("error error error error error error error error error error error error ");
+        logger.error("error error error error error error error error error error error error "); logger.error("error error error error error error error error error error error error ");
+        logger.error("error error error error error error error error error error error error ");
+        logger.error("error error error error error error error error error error error error ");
+        logger.error("error error error error error error error error error error error error ");
+        logger.error("error error error error error error error error error error error error ");
+        logger.error("error error error error error error error error error error error error ");
+        logger.error("error error error error error error error error error error error error "); logger.error("error error error error error error error error error error error error ");
+        logger.error("error error error error error error error error error error error error ");
+        logger.error("error error error error error error error error error error error error ");
+        logger.error("error error error error error error error error error error error error ");
+        logger.error("error error error error error error error error error error error error ");
+        logger.error("error error error error error error error error error error error error ");
+    }
+}
